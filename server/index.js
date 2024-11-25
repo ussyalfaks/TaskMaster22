119c94872d02e5faf5c695d808aa85b99d2c75ce
ussyalfaks <usmanalfaki@gmail.com>
2024-11-25 13:57:35 +0100

    version 2

---
+++ server/index.js
@@ -0,0 +1,44 @@
+import express from 'express';
+import mongoose from 'mongoose';
+import cors from 'cors';
+import dotenv from 'dotenv';
+import authRoutes from './routes/auth.js';
+import taskRoutes from './routes/tasks.js';
+import cookieParser from 'cookie-parser';
+
+dotenv.config();
+
+const app = express();
+
+app.use(cors({
+  origin: [
+    'https://task-master22.vercel.app',
+    'https://task-master22-6ru52ox0r-ussyalfaks-projects.vercel.app',
+    'https://task-master22-6ru52ox0r-ussyalfaks-projects.vercel.app',
+    
+  ],
+  credentials: true,
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+  allowedHeaders: ['Content-Type', 'Authorization'],
+  preflightContinue: false,
+}));
+app.options('*', cors());
+app.use(express.json());
+
+app.use(cookieParser());
+
+// Routes
+app.use('/api/auth', authRoutes);
+app.use('/api/tasks', taskRoutes);
+
+
+
+// Connect to MongoDB
+mongoose.connect(process.env.MONGODB_URI || 'mongodb+srv://usmanalfaki01:igh4XoMLqxs7xxei@taskmaster.ef3xr.mongodb.net/?retryWrites=true&w=majority&appName=taskmaster')
+  .then(() => console.log('Connected to MongoDB'))
+  .catch(err => console.error('MongoDB connection error:', err));
+
+const PORT = process.env.PORT || 10000;
+app.listen(PORT, '0.0.0.0', () => {
+  console.log(`Server running on http://0.0.0.0:${PORT}`);
+});
