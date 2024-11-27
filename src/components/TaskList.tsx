3558f32e9548c3adff4820a653c6b27d139c0ef9
ussyalfaks <usmanalfaki@gmail.com>
2024-11-27 14:26:46 +0100

    styles

--- src/components/TaskList.tsx
+++ src/components/TaskList.tsx
@@ -55,8 +55,8 @@ export function TaskList({ tasks, onTaskUpdate }: TaskListProps) {
       {tasks.map((task) => (
         <div key={task._id} className=" m-4">
           <div className="p-6 bg-white w-full hover:shadow-md transition-shadow duration-200">
-            <div className="flex flex-col w-[90%] items-start sm:items-center justify-between space-y-4 sm:space-y-0">
-              <div className="flex w-full items-start flex-col">
+            <div className="flex flex-col w-5/6 items-start sm:items-center justify-between space-y-4 sm:space-y-0">
+              <div className="flex w-full mx-2 items-start flex-col">
                   <p className={`text-lg  font-medium break-words`}>{task.title}</p>
                   <p className="text-sm  text-gray-500 break-words">{task.description}</p>
               </div>
